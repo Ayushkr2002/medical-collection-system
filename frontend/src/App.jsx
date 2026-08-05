d8409ee1782edc4d57922f74865fe103db299023
Ayush Kumar <ayush20021971@gmail.com>
2026-08-05 22:05:35 +0530

    Update before deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./layout/mainLayout"
+import MainLayout from "./layout/MainLayout"
 import DashboardLayout from "./layout/DashboardLayout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
